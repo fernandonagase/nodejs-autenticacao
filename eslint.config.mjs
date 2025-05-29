1abad747f67668010d898ea1327d643ad7c1685c
Fernando Nagase <nagase.fernando@gmail.com>
2025-05-29 19:44:00 -0300

    ignorar dist e node_modules no eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,4 +6,7 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strict,
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  }
 );
