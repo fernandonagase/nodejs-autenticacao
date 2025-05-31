ead50aadeaf4f8338aec5372c2c0c0cf6fa4158c
Fernando Nagase <nagase.fernando@gmail.com>
2025-05-31 18:45:14 -0300

    implementar classe Result

---
+++ src/tools/result.ts
@@ -0,0 +1,19 @@
+export class Result<T> {
+  ok: boolean;
+  data?: T;
+  error?: string;
+
+  constructor(ok: boolean, data?: T, error?: string) {
+    this.ok = ok;
+    this.data = data;
+    this.error = error;
+  }
+
+  static success<T>(data: T): Result<T> {
+    return new Result<T>(true, data);
+  }
+
+  static failure<T>(error: string): Result<T> {
+    return new Result<T>(false, undefined, error);
+  }
+}
