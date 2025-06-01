9b706ec0e129331a4816a2afc4fe365d206d18b3
Fernando Nagase <nagase.fernando@gmail.com>
2025-06-01 20:10:17 -0300

    criar testes para a classe Result

---
+++ src/tools/__tests__/result.ts
@@ -0,0 +1,17 @@
+import { describe, expect, it } from "@jest/globals";
+
+import { Result } from "../result.js";
+
+describe("Resultado", () => {
+  it("deve criar um resultado de sucesso", () => {
+    const result = Result.success("Dados do usuário");
+    expect(result.ok).toBe(true);
+    expect(result.data).toBe("Dados do usuário");
+  });
+
+  it("deve criar um resultado de falha", () => {
+    const result = Result.failure("Erro ao criar usuário");
+    expect(result.ok).toBe(false);
+    expect(result.error).toBe("Erro ao criar usuário");
+  });
+});
