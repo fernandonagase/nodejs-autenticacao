b15985776fe631cce7a072fa7c24af968bff05ec
Fernando Nagase <nagase.fernando@gmail.com>
2025-06-08 18:06:19 -0300

    adicionar assinatura de findByUsername em UserRepository

--- src/persistence/interfaces/user-repository.ts
+++ src/persistence/interfaces/user-repository.ts
@@ -4,5 +4,6 @@ import { Result } from "../../tools/result.js";
 export interface IUserRepository {
   create(user: User): Promise<Result<User>>;
   update(user: User): Promise<Result<void>>;
+  findByUsername(username: string): Promise<Result<User | null>>;
   findById(userId: number): Promise<Result<User | null>>;
 }
