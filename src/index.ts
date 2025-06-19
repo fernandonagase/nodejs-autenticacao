06fa3342f6cf42f8091cf354b9d9316c0aabd5e1
Fernando Nagase <nagase.fernando@gmail.com>
2025-06-19 16:03:49 -0300

    configurar o cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
 import express from "express";
+import cors from "cors";
 
 import authRouter from "./routes/auth.js";
 
 const app = express();
+
+if (!process.env.FRONTEND_URL) {
+  throw new Error("A variável de ambiente FRONTEND_URL não está definida.");
+}
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
 const PORT = process.env.PORT || 3000;
 
 app.use("/auth", authRouter);
