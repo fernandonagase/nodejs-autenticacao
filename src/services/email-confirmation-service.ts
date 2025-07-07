1aeb4625d17425a2d477a1a5c56b7a540df06975
Fernando Nagase <nagase.fernando@gmail.com>
2025-07-07 20:16:50 -0300

    refactor: usar Result2 em EmailConfirmationService

--- src/services/email-confirmation-service.ts
+++ src/services/email-confirmation-service.ts
@@ -3,7 +3,7 @@ import { readFile } from "fs/promises";
 import { fileURLToPath } from "url";
 
 import { IEmailConfirmationService } from "./interfaces/email-confirmation-service.js";
-import { Result } from "../tools/result.js";
+import { Result, resultFailure, resultSuccess } from "../tools/result2.js";
 import { MailtrapEmailService } from "./mailtrap-email-service.js";
 import { UserRepository } from "../persistence/user-repository.js";
 import { JwtEmailConfirmationAuthority } from "../domain/email-confirmation/jwt-email-confirmation-authority.js";
@@ -20,7 +20,7 @@ const EmailConfirmationService: IEmailConfirmationService = {
   ): Promise<Result<void>> {
     if (!process.env.FRONTEND_URL) {
       console.error("FRONTEND_URL não está definida");
-      return Result.failure("Não foi possível enviar o email de confirmação");
+      return resultFailure("Não foi possível enviar o email de confirmação");
     }
 
     const __filename = fileURLToPath(import.meta.url);
@@ -34,7 +34,7 @@ const EmailConfirmationService: IEmailConfirmationService = {
       html = await readFile(templatePath, "utf-8");
     } catch (error) {
       console.error("Erro ao ler template de email:", error);
-      return Result.failure("Não foi possível enviar o email de confirmação");
+      return resultFailure("Não foi possível enviar o email de confirmação");
     }
 
     html = html
@@ -50,7 +50,7 @@ const EmailConfirmationService: IEmailConfirmationService = {
   async confirmUserEmail(token: string): Promise<Result<void>> {
     if (!process.env.JWT_SECRET) {
       console.error("JWT_SECRET não está definido");
-      return Result.failure("Não foi possível confirmar o email do usuário");
+      return resultFailure("Não foi possível confirmar o email do usuário");
     }
     const validationResult = validateToken(token, process.env.JWT_SECRET);
     if (!validationResult.ok) {
@@ -58,11 +58,11 @@ const EmailConfirmationService: IEmailConfirmationService = {
         "Erro ao validar token de confirmação:",
         validationResult.error,
       );
-      return Result.failure("Não foi possível confirmar o email do usuário");
+      return resultFailure("Não foi possível confirmar o email do usuário");
     }
     if (!validationResult.data.isValid || !validationResult.data.payload) {
       console.error("Token de confirmação inválido ou mal-formado");
-      return Result.failure("Não foi possível confirmar o email do usuário");
+      return resultFailure("Não foi possível confirmar o email do usuário");
     }
     const findResult = await findEmailConfirmationByTokenId(
       validationResult.data.payload.tokenId,
@@ -72,21 +72,21 @@ const EmailConfirmationService: IEmailConfirmationService = {
         "Erro ao buscar confirmação no banco de dados:",
         findResult.error,
       );
-      return Result.failure("Não foi possível confirmar o email do usuário");
+      return resultFailure("Não foi possível confirmar o email do usuário");
     }
     if (!findResult.data) {
       console.error("Confirmação de email não encontrada");
-      return Result.failure("Não foi possível confirmar o email do usuário");
+      return resultFailure("Não foi possível confirmar o email do usuário");
     }
     if (findResult.data.userId !== validationResult.data.payload.userId) {
       console.error(
         "Usuário do token não corresponde ao usuário da confirmação",
       );
-      return Result.failure("Não foi possível confirmar o email do usuário");
+      return resultFailure("Não foi possível confirmar o email do usuário");
     }
     if (findResult.data.revoked) {
       console.error("Token revogado");
-      return Result.failure(
+      return resultFailure(
         "Confirmação de e-mail expirada. Por favor, solicite um novo link de confirmação.",
       );
     }
@@ -97,7 +97,7 @@ const EmailConfirmationService: IEmailConfirmationService = {
         "Erro ao buscar confirmação no banco de dados:",
         userResult.error,
       );
-      return Result.failure("Não foi possível confirmar o email do usuário");
+      return resultFailure("Não foi possível confirmar o email do usuário");
     }
     const user = userResult.data;
     user.verifiedEmail = true;
@@ -110,9 +110,9 @@ const EmailConfirmationService: IEmailConfirmationService = {
         "Erro ao salvar estado de verificação do usuário no banco de dados:",
         confirmationResult.error,
       );
-      return Result.failure("Não foi possível confirmar o email do usuário");
+      return resultFailure("Não foi possível confirmar o email do usuário");
     }
-    return Result.success();
+    return resultSuccess();
   },
 };
 
--- src/services/interfaces/email-confirmation-service.ts
+++ src/services/interfaces/email-confirmation-service.ts
@@ -1,4 +1,4 @@
-import { Result } from "../../tools/result.js";
+import { Result } from "../../tools/result2.js";
 
 export interface IEmailConfirmationService {
   sendConfirmationEmail(email: string, token: string): Promise<Result<void>>;
